df9aa18503260c0efb14af3b1c1cee8812e1a020
Boyang Yu <boyangy4@illinois.edu>
2020-11-06 18:21:51 -0500

    Adjusted Code following MP1 Adjustments

--- app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/activities/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/activities/MainActivity.java
@@ -51,8 +51,7 @@ public final class MainActivity extends AppCompatActivity
     super.onCreate(unused);
     Log.i("Startup", "MainActivty onCreate");
     // Retrieve the API client from the application and initiate a course summary request
-    CourseableApplication application = (CourseableApplication) getApplication();
-    application.getCourseClient().getSummary(DEFAULT_YEAR, DEFAULT_SEMESTER, this);
+
 
     // Bind to the layout in activity_main.xml
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
@@ -62,6 +61,8 @@ public final class MainActivity extends AppCompatActivity
     listAdapter.addCallback(this);
     binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
     binding.recyclerView.setAdapter(listAdapter);
+    CourseableApplication application = (CourseableApplication) getApplication();
+    application.getCourseClient().getSummary(DEFAULT_YEAR, DEFAULT_SEMESTER, this);
 
     // Register this component as a callback for changes to the search view component shown above
     // the course list
