1debc707fe0774e2ca635c2c01d31aed6c52893c
Boyang Yu <boyangy4@illinois.edu>
2020-11-06 18:44:31 -0500

    Added Course.java, Commented out errors in MP1Test.java

---
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/models/Course.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs.cs125.fall2020.mp.models;
+
+/**
+ * Course data model, inheriting Summary.
+ */
+public class Course extends Summary {
+  /**
+   * Get the description for the course object.
+   * @return string containing description.
+   */
+  public String getDescription() {
+    return null;
+  }
+}
